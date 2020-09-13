f2818e6c83159504268170d72a2b4049706fd99e
Bryan Garza <lagarzab@gmail.com>
2020-09-13 18:37:45 -0400

    Created TaskList model/migration

---
+++ database/migrations/2020_09_13_214732_create_task_lists_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaskListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task_lists', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name',100)->required;
+            $table->boolean('finished');
+            $table->string('style',200)->nullable();
+            $table->dateTime('archived', 0)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task_lists');
+    }
+}
