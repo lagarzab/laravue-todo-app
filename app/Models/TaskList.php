0adc9c0ba622aaf1511e9538755821bd9a6c12e7
Bryan Garza <lagarzab@gmail.com>
2020-09-27 10:50:13 -0400

    Updated mass assignment fields

--- app/Models/TaskList.php
+++ app/Models/TaskList.php
@@ -9,13 +9,15 @@ class TaskList extends Model
 {
     use HasFactory;
     
+    protected $gaurded = [];
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'finished', 'style', 'archived'
+        'name', 'finished', 'style', 'archived', 'user_id'
     ];
 
     public function user() {
