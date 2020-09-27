b256947a57d0325f43822e4135fd66e6386a22f8
Bryan Garza <lagarzab@gmail.com>
2020-09-27 10:49:39 -0400

    Updated task lists relationship name for user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,7 +38,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function lists() {
+    public function tasklists() {
         return $this->belongsToMany(TaskList::class);
     }
 }
