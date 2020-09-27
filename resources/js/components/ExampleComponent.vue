ccf2221f7cfb65d336cbfda82ad1ae10ed61b32b
Bryan Garza <lagarzab@gmail.com>
2020-09-27 10:11:35 -0400

    Created components for initial testing of functionality

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -6,7 +6,13 @@
                     <div class="card-header">Example Component</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                        <!-- <form action="/tasklist" method="post">
+                            <input type="hidden" name="_token" :value="csrf"> -->
+                            <button class='btn btn-primary' @click='createTaskList'>Create List</button>
+                        <!-- </form> -->
+                        <router-link :to="{name: 'login'}">Hello</router-link>
+                        <router-link :to="{name: 'tasklists'}">Task Lists</router-link>
+                        <router-link to='/none'>None</router-link>
                     </div>
                 </div>
             </div>
@@ -16,8 +22,18 @@
 
 <script>
     export default {
+        data: () => ({
+            csrf: document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+          }),
         mounted() {
             console.log('Component mounted.')
+        },
+        methods: {
+            createTaskList () {
+                axios.post('/api/tasklists')
+                    .then(res => console.log(res))
+                    .catch(err => console.error(err))
+            }
         }
     }
 </script>
---
+++ resources/js/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <label for='email'>Email Address</label>
+        <input name='email' />
+        <label for="password">Password</label>
+        <input type="password" />
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
